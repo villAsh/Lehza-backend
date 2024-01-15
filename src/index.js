726665c026a71a774411f20f0652992e47b6cc9a
vilas chauvhan <chavhanvilas02@gmail.com>
2024-01-15 14:36:04 +0530

    util code and error handling

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@
 import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
-import express from "express";
 import connectDB from "./db/index.js";
-const app = express();
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
 
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.error("MONGODB connection failed...", error));
 
 /* APPROACH 1
  (async () => {
