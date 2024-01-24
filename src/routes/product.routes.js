521c547811946748b6501caa89f2fa98d609d089
vilas chauvhan <chavhanvilas02@gmail.com>
2024-01-24 19:08:12 +0530

    feat: review and ratings

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,15 +4,16 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   getAllProducts,
   getProductDetails,
+  postReview,
 } from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.route("/get-products").get(getAllProducts);
-router.route("/get-product-details").get(getProductDetails);
+router.route("/get-product-details/:productId").get(getProductDetails);
+
 //secured routes
 router.route("/add-to-cart").post(verifyJWT, addToCart);
-
 router.route("/remove-from-cart").post(verifyJWT, removeFromCart);
-
+router.route("/get-product-details/:productId/post-comment").post(verifyJWT, postReview);
 export default router;
